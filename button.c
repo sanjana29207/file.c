a66c0c0a0572d75220e85d66c7efa506b09a9789
sanjana29207 <sanjana.a@s.amity.edu>
2025-08-10 21:58:56 +0530

    added eigth commit

--- button.c
+++ button.c
@@ -4,8 +4,8 @@
 <h1> button element </h1>
 <button type="button onclick="alert('hello world')">click me</button>
 <button type="button onclick="alert('amity university')">click me</button>
-<button type="button onclick="alert('bengalore')">click me</button>
 <button type="button onclick="alert('aset')">click me</button>
+<button type="button onclick="alert('ece')">click me</button>
 
 </body>
 </html>
