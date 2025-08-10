555855c340c709d642f588acc2d44d2145cbbd90
sanjana29207 <sanjana.a@s.amity.edu>
2025-08-10 22:02:28 +0530

    added one line

--- button.c
+++ button.c
@@ -4,9 +4,9 @@
 <h1> button element </h1>
 <button type="button onclick="alert('hello world')">click me</button>
 <button type="button onclick="alert('amity university')">click me</button>
-<button type="button onclick="alert('aset')">click me</button>
 <button type="button onclick="alert('branch')">click me</button>
 <button type="button onclick="alert('ece')">click me</button>
+<button type="button onclick="alert('cse')">click me</button>
 
 </body>
 </html>
