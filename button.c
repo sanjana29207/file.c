ff243839440a495a5739db586c4a329dbd85558f
sanjana29207 <sanjana.a@s.amity.edu>
2025-08-10 21:25:59 +0530

    added second commit

--- button.c
+++ button.c
@@ -2,6 +2,7 @@
 <html>
 <body>
 <h1> button element </h1>
-<button type="button onclick="alert('hello world')">click me</button>
+<button type="button onclick="alert()">click me</button>
+<button type="button onclick="alert('amity university')">click me</button>
 </body>
 </html>
